7e1e67d340d9fa536ce75136e679abf1bcf55832
tanahusarova <83506375+tanahusarova@users.noreply.github.com>
2021-10-25 21:25:56 +0200

    Add files via upload

---
+++ GameState.java
@@ -0,0 +1,16 @@
+package sk.uniba.fmph.dcs;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+public class GameState {
+    public List<Card> handCards;
+    public List<Pair<Integer, Card>> buyCards;
+    public int deckSize;
+    public int discardPileSize;
+    public Optional<Card> discardPileTop;
+    public int points;
+    public int turn;
+}
---
+++ SimpleDominionInterface.java
@@ -0,0 +1,10 @@
+package sk.uniba.fmph.dcs;
+
+import java.util.Optional; 
+ 
+interface SimpleDominionInterface {
+    public Optional<GameState> playCard(int handIdx);
+    public Optional<GameState> endPlayCardPhase();
+    public Optional<GameState> buyCard(int buyCardIdx);
+    public Optional<GameState> endTurn();
+}
